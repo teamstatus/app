a8a694fee3589234a9a2b2271341921a4a04ed87
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-06-06 09:02:18 +0200

    feat(login): block buttons while loading

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -5,6 +5,7 @@ import { InternalError, type ProblemDetail } from '../context/Projects.js'
 export const Login = () => {
 	const { setUser } = useAuth()
 	const [success, setSuccess] = useState<string | undefined>()
+	const [loading, setLoading] = useState<boolean>(false)
 	const [error, setError] = useState<ProblemDetail>()
 	const [email, setEmail] = useState('')
 	const [pin, setPIN] = useState('')
@@ -57,8 +58,9 @@ export const Login = () => {
 												'btn-outline-primary': isEmailValid,
 												'btn-outline-secondary': !isEmailValid,
 											})}
-											disabled={!isEmailValid}
+											disabled={!isEmailValid || loading}
 											onClick={() => {
+												setLoading(true)
 												setSuccess(undefined)
 												setError(undefined)
 												setPIN('')
@@ -82,6 +84,9 @@ export const Login = () => {
 													.catch((error) => {
 														console.error(error)
 														setError(InternalError(error.message))
+													})
+													.finally(() => {
+														setLoading(false)
 													})
 											}}
 										>
@@ -110,12 +115,13 @@ export const Login = () => {
 									<div class="ms-3">
 										<button
 											type="submit"
-											disabled={!isPINValid}
+											disabled={!isPINValid || loading}
 											class={cx('btn text-nowrap', {
 												'btn-primary': isPINValid,
 												'btn-secondary': !isPINValid,
 											})}
 											onClick={() => {
+												setLoading(true)
 												setError(undefined)
 												setSuccess(undefined)
 												fetch(`${API_ENDPOINT}/login/email/pin`, {
@@ -150,6 +156,10 @@ export const Login = () => {
 													.catch((error) => {
 														console.error(error)
 														setError(InternalError(error.message))
+													})
+
+													.finally(() => {
+														setLoading(false)
 													})
 											}}
 										>
