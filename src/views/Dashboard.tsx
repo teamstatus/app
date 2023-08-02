3ef64c0f13a4d97a77fcb87ccda4cd81a681040e
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-08-02 22:12:18 +0200

    feat(invitations): remove manual invitation accepting

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -5,7 +5,6 @@ import { LogoHeader } from '#components/LogoHeader.js'
 import { route } from 'preact-router'
 import { ProjectMenu } from '#components/ProjectMenu.js'
 import { Main } from '#components/Main.js'
-import { AcceptProjectInvitation } from '#components/AcceptProjectInvitation.js'
 import { Invitations } from '#components/Invitations.js'
 
 export const Dashboard = ({ redirect }: { redirect?: string }) => {
@@ -52,11 +51,6 @@ export const Dashboard = ({ redirect }: { redirect?: string }) => {
 						</div>
 					</section>
 				)}
-				<div class="row mt-3">
-					<div class="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
-						<AcceptProjectInvitation />
-					</div>
-				</div>
 				<div class="row mt-3">
 					<div class="col-12 col-md-8 offset-md-2 col-lg-6 offset-lg-3">
 						<Invitations />
